72cf45c2bec41910230ed2736f813c881073018d
Logan Endes <82857250+Log45@users.noreply.github.com>
2024-04-08 20:25:06 -0400

    now accepts mp4 as input

--- OpenAI-Notetaker/app.py
+++ OpenAI-Notetaker/app.py
@@ -4,13 +4,20 @@ from pathlib import Path
 from dataclasses import dataclass
 from audio_splitter import split_audio
 import argparse
+from moviepy.editor import *
 
 @dataclass
 class Lecture:
   audio_file: str
   audio_size: float
   split_list: list
-  
+
+def mp4_to_mp3(mp4: str, mp3: str):
+  file = AudioFileClip(mp4)
+  file.write_audiofile(mp3)
+  file.close()
+  return mp3
+
 def transcribe_audio(lecture: Lecture) -> str:
   transcription = ""
   if lecture.split_list != []:
@@ -50,6 +57,10 @@ def main():
   client = OpenAI(api_key=opt.api_key) # instance of OpenAI used by transcriber and notetaker
 
   audio_file = opt.audio_file
+
+  if ".mp4" in audio_file: # change this to a struct with all compatible video types eventually...
+    audio_file = mp4_to_mp3(audio_file, f"{file_stem}.mp3")
+
   audio_path = Path(audio_file)
   file_stem = audio_path.stem
   output_dir = f"{os.getcwd()}/{file_stem}"
@@ -79,3 +90,4 @@ def main():
 
 if __name__ == "__main__":
   main()
+  
